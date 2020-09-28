5a584215ccfefc0ce98c3022da7a2ebf27d4c0af
MichaelNguyen756 <silverwolf@MickyHome-PC.localdomain>
2020-09-29 01:52:01 +1000

    Delete unsused import

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -3,7 +3,6 @@ import _ from 'lodash';
 import { filterByVersionGroup } from '.';
 import {
     PokemonAPIResourceList,
-    PokemonType,
     PokemonAbility,
     PokemonMove,
     PokemonMoveVersion,
